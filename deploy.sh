6745ec31f305852227716fd48c5ae8a3ef8f1e04
Pooja M <42378263+PoojaM22@users.noreply.github.com>
2024-03-21 14:00:57 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker login -u pooja942 -p dckr_pat_urG82l6cCVDQoUCF88Kbz3_P3Kw
 
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
@@ -7,12 +8,10 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
 
 if [[ $GIT_BRANCH == "origin/master" ]]; then
 
-        docker tag test pooja942/prod
+        docker tag nginx pooja942/prod
         docker push pooja942/prod
 
 else
         echo "failed"
 fi
-~                                                                                                                                                                                                                  
-~                                                                                                                                                                                                                  
-~                    
+                  
