6e45cac7d26f27805ecd644f19d8fe82e5109b58
Pooja M <42378263+PoojaM22@users.noreply.github.com>
2024-03-21 13:39:23 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+docker login -u pooja942 -p dckr_pat_urG82l6cCVDQoUCF88Kbz3_P3Kw
+
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+
+        docker tag nginx pooja942/dev
+        docker push pooja942/dev
+
+if [[ $GIT_BRANCH == "origin/master" ]]; then
+
+        docker tag test pooja942/prod
+        docker push pooja942/prod
+
+else
+        echo "failed"
+fi
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                    
